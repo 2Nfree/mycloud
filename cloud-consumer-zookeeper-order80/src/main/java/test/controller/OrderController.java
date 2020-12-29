20a6af7ec69f725d515a77a5fe63429e6416627d
2Nfree <1093278167@qq.com>
2020-12-29 15:50:47 +0800

    Initial commit

---
+++ cloud-consumer-zookeeper-order80/src/main/java/test/controller/OrderController.java
@@ -0,0 +1,40 @@
+package test.controller;
+
+import com.test.entities.CommonResult;
+import com.test.entities.Payment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class OrderController {
+
+    //public static final String PAYMENT_URL="http://localhost:8001";
+    public static final String PAYMENT_URL="http://cloud-provider-payment";
+
+    @Resource
+    public RestTemplate restTemplate;
+
+    @GetMapping("/consumer/payment/create/zk")
+    public String paymentInfo(){
+        return restTemplate.getForObject(PAYMENT_URL+"/payment/zk",String.class);
+    }
+
+    @GetMapping("/consumer/payment/create")
+    public CommonResult<Payment> create(@RequestBody Payment payment){
+
+        return restTemplate.postForObject(PAYMENT_URL+"/payment/create",payment,CommonResult.class);
+
+    }
+    @GetMapping("/consumer/payment/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id){
+
+        return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
+    }
+}
---
+++ cloud-provider-payment8004/src/main/java/com/test/controller/PaymentController.java
@@ -0,0 +1,52 @@
+package com.test.controller;
+
+import com.test.entities.CommonResult;
+import com.test.entities.Payment;
+import com.test.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.UUID;
+
+@RestController
+@Slf4j
+public class PaymentController {
+    @Resource
+    private PaymentService paymentService;
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @RequestMapping(value="/payment/zk")
+    public String PaymentZK(){
+
+        return "SpringCloud with Zookeeper："+serverPort+"\t"+ UUID.randomUUID().toString();
+    }
+
+    @PostMapping(value = "/payment/create")
+    public CommonResult create(@RequestBody Payment payment){
+        int result = paymentService.create(payment);
+        log.info("____插入结果:"+ result);
+        if (result>0){
+            return new CommonResult(200,"插入成功，端口号："+serverPort,result);
+        }else{
+            return new CommonResult(400,"插入失败，端口号："+serverPort,null);
+        }
+
+    }
+
+    @GetMapping(value = "/payment/get/{id}")
+    public CommonResult getPaymentById(@PathVariable("id") Long id){
+        Payment payment = paymentService.getPaymentById(id);
+        log.info("____查询结果"+payment);
+        if (payment!=null){
+            return new CommonResult(200,"查询成功，端口号："+serverPort,payment);
+        }else{
+            return new CommonResult(400,"没有对应记录，查询id："+id+"，端口号："+serverPort,null);
+        }
+
+    }
+
+}
