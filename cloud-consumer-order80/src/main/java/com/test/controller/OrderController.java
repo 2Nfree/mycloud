5596bd2bf2969484258bd3ddc244c387a8f21a0c
2Nfree <1093278167@qq.com>
2021-01-14 15:06:05 +0800

    consul实现注册中心

--- cloud-consumer-order80/src/main/java/com/test/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/test/controller/OrderController.java
@@ -3,10 +3,8 @@ package com.test.controller;
 import com.test.entities.CommonResult;
 import com.test.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
@@ -16,20 +14,45 @@ import javax.annotation.Resource;
 public class OrderController {
 
     //public static final String PAYMENT_URL="http://localhost:8001";
-    public static final String PAYMENT_URL="http://CLOUD-PAYMENT-SERVICE";
+    public static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
 
     @Resource
     public RestTemplate restTemplate;
 
-    @GetMapping("/consumer/payment/create")
-    public CommonResult<Payment> create(@RequestBody Payment payment){
-
-        return restTemplate.postForObject(PAYMENT_URL+"/payment/create",payment,CommonResult.class);
+    @PostMapping("/consumer/payment/create")
+    public CommonResult<Payment> create(@RequestBody Payment payment) {
+        return restTemplate.postForObject(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
 
     }
-    @GetMapping("/consumer/payment/get/{id}")
-    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id){
 
-        return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
+    @PostMapping("/consumer/payment/createEntity")
+    public CommonResult<Payment> createEntity(@RequestBody Payment payment) {
+
+        ResponseEntity<CommonResult> entity = restTemplate.postForEntity(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
+        if (entity.getStatusCode().is2xxSuccessful()){
+            return entity.getBody();
+        }else{
+            return new CommonResult(400,"error");
+        }
+
+    }
+
+    @GetMapping("/consumer/payment/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id) {
+
+        return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+    }
+
+
+
+    @GetMapping("/consumer/payment/getEntity/{id}")
+    public CommonResult<Payment> getPaymentEntityById(@PathVariable("id") Long id) {
+
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+        if (entity.getStatusCode().is2xxSuccessful()){
+            return entity.getBody();
+        }else{
+            return new CommonResult(400,"error");
+        }
     }
 }
